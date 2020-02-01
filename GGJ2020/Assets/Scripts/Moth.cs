c10e6970ff25d5d2a5ea88e8e1900cafb5641a51
Jyckle <jycklepasquesi@gmail.com>
2020-02-01 17:42:39 -0600

    updated moth

---
+++ GGJ2020/Assets/Scripts/Moth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moth : GenericEnemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        damageAmount = 20;
+        maxHealth = 30;
+        moveRate = 5f;
+        maxRange = 20;
+        minRange = 4;
+        backupDistance = 1;
+        backupRate = 2;
+        attackDistance = 5;
+        attackRate = 25;
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        base.Update();
+    }
+}
