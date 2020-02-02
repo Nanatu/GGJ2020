4e5c4ad5be9582de540b8216b164952fcd6a9b14
William Norris <emcskid@gmail.com>
2020-02-02 12:21:49 -0600

    Changed Player Attack script - WZN

--- GGJ2020/Assets/Scripts/ImportedScripts/PlayerAttack.cs
+++ GGJ2020/Assets/Scripts/ImportedScripts/PlayerAttack.cs
@@ -24,7 +24,6 @@ public class PlayerAttack : MonoBehaviour
         faceDir = this.transform.parent.GetComponent<Controller2D>().collisions.faceDir;
         if (col.gameObject.tag == "Enemy" && !currentCollisions.Contains(col.gameObject))
         {
-            SoundManagerScript.PlaySound("mubAttack");
             currentCollisions.Add(col.gameObject);
             col.gameObject.SendMessage("TakeDamage", 1);
             // col.gameObject.SendMessage("HitMove", new Vector2(-5.0f, 0.0f));
@@ -32,8 +31,8 @@ public class PlayerAttack : MonoBehaviour
 
             if (gameObject.name == "LightAttack")
             {
-                SoundManagerScript.PlaySound("mubAttack");
                 col.gameObject.SendMessage("HitMove", new Vector2(faceDir * 25.0f, 0.0f));
+                SoundManagerScript.PlaySound("mubAttack");
             }
         }
 
